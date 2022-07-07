38c6dab09b4a448e85d442199e29a9b3a4c5dd9b
mikeJT <jeerasaktarat@gmail.com>
2022-07-07 14:01:28 +0700

    creadted component: premium & details art:4 .9

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Shoes',
-            brand: 'SE 331',
-            onSale: false,
-            inventory: 100,
-            details: ['50% cotton', '30% wool', '20% polyester'],
-            variants: [
-                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg',quantity: 50 },
-                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg',quantity: 0 }
-            ],
-            selectedVariant: 0,
-            cart: 0
-
+            // product: 'Shoes',
+            // brand: 'SE 331',
+            // onSale: false,
+            // inventory: 100,
+            // details: ['50% cotton', '30% wool', '20% polyester'],
+            // variants: [
+            //     { id: 2234, color: 'green', image: './assets/images/socks_green.jpg',quantity: 50 },
+            //     { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg',quantity: 0 }
+            // ],
+            // selectedVariant: 0,
+            cart: 0,
+            premium: true
         }
     },
     methods: {
